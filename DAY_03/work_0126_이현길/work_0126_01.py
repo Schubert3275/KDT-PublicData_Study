ced022483c61c29a8a6f819ae9e4fb375b804319
Schubert3275 <dragon3275@gmail.com>
2024-01-26 16:52:06 +0900

    공공데이터 3일차

--- DAY_03/work_0126_이현길/work_0126_01.py
+++ DAY_03/work_0126_이현길/work_0126_01.py
@@ -16,7 +16,7 @@ with open('gender.csv', encoding='utf-8-sig') as f:
     for row in data:
         if city in row[0]:
             city_name = row[0]
-            male_count = int(row[104].replace(',',''))
+            male_count = int(row[104].replace(',', ''))
             female_count = int(row[207].replace(',', ''))
             plt.subplot(3, 3, count)
             plt.pie([male_count, female_count], labels=['남성', '여성'], autopct='%.1f%%', startangle=90)
