8bf68909ee2628becb4f490209cef4bb1c4155f8
Schubert3275 <dragon3275@gmail.com>
2024-01-26 16:51:39 +0900

    공공데이터 3일차

--- DAY_03/work_0126_이현길/work_0126_01.py
+++ DAY_03/work_0126_이현길/work_0126_01.py
@@ -8,25 +8,23 @@ import koreanize_matplotlib
 
 with open('gender.csv', encoding='utf-8-sig') as f:
     data = csv.reader(f)
-    population = []
     city = '대구광역시'
-    city_name = ''
-    color = ['cornflowerblue', 'tomato']
+    count = 1
 
     plt.figure(figsize=(12, 8))
-    for i in range(9):
-        male_count = 0
-        female_count = 0
-        for row in data:
-            if city in row[0]:
-                city_name = row[0]
-                for j in range(106, 207):
-                    male_count += int(row[j].replace(',', ''))
-                    female_count += int(row[j + 103].replace(',', ''))
-                break
-        plt.subplot(3, 3, i + 1)
-        plt.pie([male_count, female_count], labels=['남성', '여성'], autopct='%.1f%%', startangle=90)
-        plt.title(city_name)
+    color = ['cornflowerblue', 'tomato']
+    for row in data:
+        if city in row[0]:
+            city_name = row[0]
+            male_count = int(row[104].replace(',',''))
+            female_count = int(row[207].replace(',', ''))
+            plt.subplot(3, 3, count)
+            plt.pie([male_count, female_count], labels=['남성', '여성'], autopct='%.1f%%', startangle=90)
+            plt.title(city_name)
+            count += 1
+        if count > 9:
+            break
+
 plt.suptitle(city + " 구별 남녀 인구 비율")
 plt.tight_layout()
 plt.show()
